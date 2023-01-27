fb65433c147f2e8777b885cba12b18ee9eaad923
Ritik <raj769417@gmail.com>
2023-01-27 21:09:13 +0530

    added route to update the list

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Task = require('./models/task');
+const Task_History = require('./models/tasks_history');
+
 const methodOverride = require('method-override');
 
 mongoose.set('strictQuery', true);
@@ -19,7 +21,6 @@ app.use(methodOverride('_method'));
 
 app.get('/todos', async (req, res) => {
     const tasks = await Task.find({});
-    console.log(tasks);
     res.render('showList', { tasks });
 })
 
@@ -27,16 +28,30 @@ app.post('/todos', async (req, res) => {
     const newTask = req.body;
     const T1 = new Task(newTask);
     await T1.save();
-    const tasks = await Task.find({});
     res.redirect('/todos');
 })
 
 app.delete('/todos/:id', async (req, res) => {
     const { id } = req.params;
+    console.log("deleted.....................")
     const getTask = await Task.findByIdAndDelete(id);
     console.log(getTask);
+
     res.redirect('/todos');
 })
+
+app.post('/todos/update/:id', async (req, res) => {
+    const { id } = req.params;
+    
+    const getTask = await Task.findById(id);
+    
+    const T1 = new Task_History({title: getTask.title, category: getTask.category});
+    await T1.save();
+
+    const g = await Task.findByIdAndDelete(id);
+    res.redirect('/todos');
+})
+
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000 ...");
